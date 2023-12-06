576d0a2ba7c775b421673006ac1733aa31635678
Gabriel Garcia <gabriel.garrcia23@outlook.com>
2023-12-06 14:04:05 -0300

    cleanpages

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,7 @@ module.exports = {
   darkMode: 'media',
   theme: {
     extend: {
-      colors: {
-        brand: '#c532f5',
-        jubileu: {
-          100: '#39edd8',
-          200: '#30c9b8',
-          300: '#27a395',
-          400: '#1d7a70'
-        }
+
       }
     },
   },
